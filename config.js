00400eaa4f3d3168f18cdaccaf1880150fd9437b
Hacker127367 <villagef335@gmail.com>
2025-01-22 08:14:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    SESSION_ID: process.env.SESSION_ID || "ITN3VIRb#_cAs8HO6UQBJCL51iP2wvrL1U8mTL-IvGL7XARNV7hw",
     CAPTION: process.env.CAPTION || "POWERED BY SOBIA BUTT",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
